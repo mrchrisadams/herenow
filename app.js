1a272e83b142305908ee0f84a0fe4cf294a78504
Chris Adams <wave@chrisadams.me.uk>
2010-05-26 21:54:38 +0100

    Added extra modules, and express

--- app.js
+++ app.js
@@ -1,60 +1,16 @@
 GLOBAL.DEBUG = true;
 
-sys = require("sys");
-test = require("mjsunit");
+sys = require("sys"),
+path = require('path'),
+mongo = require('./lib/mongoose/mongoose').Mongoose;
 
-var mongo = require('/home/james/Desktop/Work/HereNow/HereNow2/deps/node-mongodb-native/lib/mongodb');
-
-var query = function(callback){
-  var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
-  var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : mongo.Connection.DEFAULT_PORT;
-  var result;  
-  sys.puts("Connecting to " + host + ":" + port);
-  try {
-    var db = new mongo.Db('test', new mongo.Server(host, port, {}), {});
-  } catch (e) {
-    sys.puts('Caught')
-    return callback(e)
-  }
-  db.open(function(error, db) {
-    if (error) callback(error)
-    else {
-      db.collection('foo', function(error, collection) {
-        if (error) callback(error)
-        else {
-          collection.find(function(error, cursor) {
-            if (error) callback(error)
-            else {
-              cursor.toArray(function(err, found) {
-                sys.puts("Printing docs from Array")
-                callback(null, found);
-                found.forEach(function(item) {
-                  sys.puts("Doc from Array " + sys.inspect(item));
-                });
-              });
-            }
-          });
-        }
-      });
-    }
-  });
-};
-
-
-//http = require('http')
-//http.createServer(function(request, response) {
-//  response.writeHead(200, {'Content-Type': 'text/html'});
-//  query(function(error, result) {
-//    if (error) {
-//      response.end('Failed: '+error);
-//    } else {
-//      response.end('Success: '+result[0].a);
-//    }
-//  });
-//}).listen(3000)
+// .... got sick of the git submodule madness of express
+// express = require('./lib/express/lib/express');
 
 var kiwi = require('kiwi');
 kiwi.require('express');
+
+
 
 
 
@@ -70,21 +26,6 @@ var keys = function(obj) {
     }
     return output;
 }
-//// This is a public page a user would visit
-//var presence = function(req, resp){
-//  var external_ip = req.environ['REMOTE_ADDR']
-//  var checksum = generate_checksum();
-//  db.query(
-//    'INSERT INTO presence (checksum) VALUES (?);', 
-//    (checksum),
-//  )
-//  var iframes = db.query(
-//    'SELECT internal_ip FROM box WHERE external_ip = ?', 
-//    (external_ip,)
-//  )
-//  return 'presence.html should be returned with checksum: '+checksum+' and frames: '+iframes
-//}
-
 
 var box = {
   box_id: 1,
