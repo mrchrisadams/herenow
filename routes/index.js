e4fcbd4a30738b24fe9ed4b14c46fa0141001ec3
James Smith <james@floppy.org.uk>
2012-09-04 12:13:45 +0100

    Only show devices with IPs

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ exports.index = function(req, res){
     { ip: "192.168.1.8", mac : "00:1a:a2:a6:d7:2c", name: "HP Printer", type: 'printer' },
     { ip: "192.168.1.8", mac : "00:1a:a2:a1:a3:3a", name: "Netgear Router", type: 'router' },
     { ip: "192.168.1.5", mac : "00:1e:c2:a4:d7:2e", name: "Chris' iPhone", type: 'phone', user: "mrchrisadams" },
-    { ip: "192.168.1.6", mac : "00:1a:a2:e4:d3:1a", user: "mrchrisadams", type: 'laptop' },
+    { mac : "00:1a:a2:e4:d3:1a", user: "mrchrisadams", type: 'laptop' },
     { ip: "192.168.1.7", mac : "00:1a:a6:a6:d7:2d" },
   ];
   
@@ -27,11 +27,11 @@ exports.index = function(req, res){
     
     // split into owned and ownerless devices
     ownedDevices : allDevices.filter(function hasOwner(element, index, array) {
-      return (element['user'] != null);
+      return (element['user'] != null && element['ip'] != null);
     }),
     
     ownerlessDevices : allDevices.filter(function hasNoOwner(element, index, array) {
-      return (element['user'] == null);
+      return (element['user'] == null && element['ip'] != null);
     })
 
   })
